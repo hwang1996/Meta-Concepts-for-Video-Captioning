395ff6e61aa56a4de2c63bed016fbb35591cdfb8
hwang1996 <hao.wang9605@gmail.com>
2020-12-01 22:25:17 +0700

    update

--- msr-vtt/captioning/run_train.sh
+++ msr-vtt/captioning/run_train.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=0 python misc/train.py \
+CUDA_VISIBLE_DEVICES=1 python misc/train.py \
 			--checkpoint_path ../model/run \
 			--model_file ../model/run/model.pth \
 			--best_model_file ../model/run/model_best.pth \
 			--result_file ../model/run/test_results.json \
-			--start_from ../model/run/model.pth \
+			--start_from ../model/run/model_.pth \
 			--batch_size 32 \
 			--rnn_num_layers 1 \
 			--learning_rate 8e-5 \
 			--lr_update 100 \
 			--lr_decay_rate 0.5 \
-			--max_epochs 150 \
+			--max_epochs 55 \
 			--beam_size 5 \
 			--use_rl=1 \
 			--use_cst=1 \

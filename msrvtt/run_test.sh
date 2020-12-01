42b2d446f34bc037b791ff4534447a3508062655
hwang1996 <hao.wang9605@gmail.com>
2020-12-01 10:56:21 +0700

    coco

--- msrvtt/run_test.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python misc/test.py \
-		--result_file output/model/expand/all_rl/test_results.json \
-		--model_file output/model/expand/all_rl/model_best.pth \
-		--rnn_num_layers 1 \
-		--beam_size 5 \
